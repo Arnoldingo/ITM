d68ea898a67b80a67e5610bdc60087a574cc3bc9
Arnoldingo <571291@student.saxion.nl>
2026-01-14 10:59:26 +0100

    Add frontend with ITM logo

---
+++ frontend/EasyDevOpsApp/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyDevOpsApp.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
